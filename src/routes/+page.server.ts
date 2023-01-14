a4870b9893d3d87bee27c3309f84eb4a90bbf2e8
florentazd <florent4real@yahoo.com>
2023-01-14 11:56:19 +0100

    update the main page ui

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,14 @@
+import { convertTime } from '$lib/utils/dateConverter';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
 
     const jobs = await locals.pb.collection("jobs").getList(1, 5, {
         filter: 'status = "En ligne"', expand: 'organization', sort: "-created"
+    })
+
+    jobs.items.forEach((item: any) => {
+        item.created = convertTime(item.created)
     })
     return {
         jobs: JSON.parse(JSON.stringify(jobs))
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import { PUBLIC_API_URL } from '$env/static/public';
 	import type { PageData } from './$types';
 	export let data: PageData;
@@ -131,110 +132,123 @@
 		</div>
 	</div>
 	<!--  -->
-	<div class="z-50 w-full h-fit min-h-[50vh] mx-auto flex flex-col gap-y-4 pb-10 px-4 bg-[#F8F9FD]">
-		<h5 class="font-semibold sm:w-[70vw] mx-auto text-left pt-4 text-[#34495e]">
-			Dernières offres
-		</h5>
-		<div id="offers" class="flex flex-col gap-y-4 w-full sm:w-[70vw] mx-auto min-h-[280px]">
+	<div
+		class="z-50 w-full h-fit min-h-[50vh] mx-auto flex flex-col gap-y-4 pb-10 px-4 sm:px-16 bg-[#F8F9FD]"
+	>
+		<h5 class="font-semibold mx-auto text-left pt-4 text-[#34495e] w-full">Dernières offres</h5>
+		<div id="offers" class="flex flex-wrap  gap-10 w-full mx-auto min-h-[280px]">
 			{#each jobs.items as job}
-				<a
-					href={'/jobs/' + job.id}
-					class="h-28 w-full border-[1px] rounded-md bg-white grid grid-cols-[60px,1fr] sm:grid-cols-[80px,1fr] px-4 sm:px-4 gap-x-2"
+				<div
+					in:fade
+					class="h-fit w-full sm:w-72 flex flex-col  border-[1px] rounded bg-white p-4 gap-4"
 				>
-					<div class="h-full w-full flex justify-center items-center">
-						<img
-							class="w-[60px] h-[60px] sm:h-[60px] rounded-md object-cover"
-							src={job.expand.organization.logo
-								? `${PUBLIC_API_URL}/api/files/organizations/${job.expand.organization.id}/${job.expand.organization.logo}`
-								: '/images/empty.jpg'}
-							alt=""
-						/>
-					</div>
-					<div class="w-full h-full flex flex-col justify-center py-2">
-						<h3 class="text-gray-900 font-bold text-base sm:text-lg">{job.name}</h3>
-						<p class="flex mb-1 items-center gap-1">
-							<span class="capitalize gap-x-1 flex items-center text-sm font-semibold text-gray-500"
-								>{job.expand.organization.name}</span
-							>
-							{#if job.expand.organization.badge}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="#3498db"
-									class="w-4 h-4"
+					<div class="grid grid-cols-[60px,1fr] gap-x-2">
+						<div>
+							<img
+								class="w-[60px] h-[60px] sm:h-[60px] rounded-md object-cover"
+								src={job.expand.organization.logo
+									? `${PUBLIC_API_URL}/api/files/organizations/${job.expand.organization.id}/${job.expand.organization.logo}`
+									: '/images/empty.jpg'}
+								alt=""
+							/>
+						</div>
+						<div class="overflow-x-hidden flex flex-col justify-center">
+							<h3 class="text-gray-900 font-bold text-base w-full sm:text-lg	whitespace-nowrap">
+								{job.name}
+							</h3>
+							<p class="flex mb-1 items-center gap-1">
+								<span
+									class="capitalize gap-x-1 flex items-center text-sm font-semibold text-gray-500"
+									>{job.expand.organization.name}</span
 								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-									/>
-								</svg>
-							{/if}
-						</p>
-						<div class="flex gap-x-4 text-sm">
-							<div class="flex gap-1 items-center">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="hidden sm:flex w-4 h-4"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-									/>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-									/>
-								</svg>
-								<span>{job.location}</span>
-							</div>
-							<!--  -->
-							<div class="gap-1 items-center hidden sm:flex">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-4 h-4"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
-									/>
-								</svg>
-								<span>{job.contract}</span>
-							</div>
-							<!--  -->
-							<div class="gap-1 items-center hidden sm:flex">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-4 h-4"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
-									/>
-								</svg>
-								<span>{job.salary}</span>
-							</div>
+								{#if job.expand.organization.badge}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="#3498db"
+										class="w-4 h-4"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
+										/>
+									</svg>
+								{/if}
+							</p>
 						</div>
 					</div>
-				</a>
+					<!--  -->
+					<p class="text-sm">{job.created}</p>
+					<!--  -->
+					<div class="flex gap-x-4 text-sm flex-col gap-y-4">
+						<div class="flex gap-1 items-center">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="flex w-4 h-4"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+								/>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+								/>
+							</svg>
+							<span>{job.location}</span>
+						</div>
+
+						<div class="gap-1 items-center flex">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-4 h-4"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+								/>
+							</svg>
+							<span>{job.contract}</span>
+						</div>
+
+						<div class="gap-1 items-center flex">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-4 h-4"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+								/>
+							</svg>
+							<span>{job.salary}</span>
+						</div>
+						<a
+							href={'/jobs/' + job.id}
+							class="border rounded border-[#3498db] text-[#3498db] h-10 w-full flex justify-center items-center"
+							>Consulter l'offre</a
+						>
+					</div>
+				</div>
 			{:else}
 				<i>Aucune offre disponible pour le moment.</i>
 			{/each}
